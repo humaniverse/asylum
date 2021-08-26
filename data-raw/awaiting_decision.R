3455342c9405834cd01c3ccdd05c62fc310d192d
Matt Thomas <matthewgthomas@gmail.com>
2021-08-26 17:48:27 +0100

    Added data on people awaiting decisions

---
+++ data-raw/awaiting_decision.R
@@ -0,0 +1,35 @@
+library(tidyverse)
+library(lubridate)
+library(devtools)
+library(readxl)
+library(httr)
+
+load_all()
+
+query_url <-
+  query_urls |>
+  filter(data_set == "awaiting_decision") |>
+  pull(query_url)
+
+GET(
+  query_url,
+  write_disk(tf <- tempfile())
+)
+
+awaiting_decision <-
+  read_excel(tf, sheet = "Data - Asy_D03", skip = 1)
+
+# Wrangling
+awaiting_decision <-
+  awaiting_decision |>
+  rename(Date = `Date (as at…)`) |>
+  filter(Date != "End of table") |>
+  mutate(Date = dmy(Date)) |>
+
+  mutate(
+    `Applicant type` = if_else(`Applicant type` == "Dependant", "Dependant", "Main applicant")
+  )
+
+# Save output to data/ folder
+usethis::use_data(awaiting_decision, overwrite = TRUE)
+readr::write_csv(awaiting_decision, "data-raw/awaiting_decision.csv")
