56b35595cc9f64a7db48035c930c8011ba778b41
Matthew Gwynfryn Thomas <matthewgthomas@gmail.com>
2021-08-30 18:43:28 +0100

    Fixed bug in outcomes column naming and documentation

--- data-raw/outcomes.R
+++ data-raw/outcomes.R
@@ -19,6 +19,9 @@ GET(
 outcomes <-
   read_excel(tf, sheet = "Data - Asy_D04", skip = 1)
 
+# Rename this column because R package documentation doesn't seem to like commas in column names
+names(outcomes)[names(outcomes) == "Final outcomes: Refused asylum, HP, DL or other leave"] <- "Final outcomes: Refused asylum or HP or DL or other leave"
+
 # Save output to data/ folder
 usethis::use_data(outcomes, overwrite = TRUE)
 readr::write_csv(outcomes, "data-raw/outcomes.csv")
