00ddf3a2ab8cc7903e7d9f813678cfbd54aa64d1
Matthew Gwynfryn Thomas <matthewgthomas@gmail.com>
2023-03-31 16:11:49 +0100

    Add more data and fix column names

--- R/write-db.R
+++ R/write-db.R
@@ -2,18 +2,22 @@
 #' @param db_path Where to save the SQLite database
 #' @param db_filename SQLite filename
 #' @export
-save_sqlite <- function(db_path, db_filename = "asylum.sqlite") {
+save_sqlite <- function(db_path = ".", db_filename = "asylum.db") {
   db <- file.path(db_path, db_filename)
 
   con <- RSQLite::dbConnect(RSQLite::SQLite(), db)
 
-  RSQLite::dbWriteTable(con, "applications", asylum::applications, indexes = list("Date", "Year", "Quarter", "Nationality"))
-  RSQLite::dbWriteTable(con, "decisions_resettlement", asylum::decisions_resettlement, indexes = list("Date", "Year", "Quarter", "Nationality"))
-  RSQLite::dbWriteTable(con, "family_reunion", asylum::family_reunion, indexes = list("Date", "Year", "Quarter", "Nationality"))
+  applications <- asylum::applications |> dplyr::rename_with(~ gsub(" ", "_", .x, fixed = TRUE))
+  decisions_resettlement <- asylum::decisions_resettlement |> dplyr::rename_with(~ gsub(" ", "_", .x, fixed = TRUE))
+  family_reunion <- asylum::family_reunion |> dplyr::rename_with(~ gsub(" ", "_", .x, fixed = TRUE))
+  awaiting_decision <- asylum::awaiting_decision |> dplyr::rename_with(~ gsub(" ", "_", .x, fixed = TRUE))
+  irregular_migration <- asylum::irregular_migration |> dplyr::rename_with(~ gsub(" ", "_", .x, fixed = TRUE))
 
-  # dplyr::copy_to(con, asylum::applications, "applications", indexes = list("Date", "Year", "Quarter", "Nationality"))
-  # dplyr::copy_to(con, asylum::decisions_resettlement, "decisions_resettlement", indexes = list("Date", "Year", "Quarter", "Nationality"))
-  # dplyr::copy_to(con, asylum::family_reunion, "family_reunion", indexes = list("Date", "Year", "Quarter", "Nationality"))
+  RSQLite::dbWriteTable(con, "applications", applications, indexes = list("Date", "Year", "Quarter", "Nationality"))
+  RSQLite::dbWriteTable(con, "decisions_resettlement", decisions_resettlement, indexes = list("Date", "Year", "Quarter", "Nationality"))
+  RSQLite::dbWriteTable(con, "family_reunion", family_reunion, indexes = list("Date", "Year", "Quarter", "Nationality"))
+  RSQLite::dbWriteTable(con, "awaiting_decision", awaiting_decision, indexes = list("Date", "Nationality"))
+  RSQLite::dbWriteTable(con, "irregular_migration", irregular_migration, indexes = list("Date", "Year", "Quarter", "Nationality"))
 
   RSQLite::dbDisconnect(con)
 }
